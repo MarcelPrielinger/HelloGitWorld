370c0a8add34727f5b19c93f3f5a800f6fecfacc
Marcel Prielinger <marcel.prielinger@htl-wels.at>
2020-10-08 10:42:41 +0200

    Added Try/Catch

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -1,9 +1,29 @@
 package Main;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Main
 {
     public static void main(String[] args)
     {
-        System.out.println("Hello World!");
+        try {
+            Scanner scanner = new Scanner(System.in);
+            int no_1;
+            int no_2;
+
+            System.out.printf("Geben Sie bitte einen Werte ein: ");
+            no_1 = scanner.nextInt();
+            System.out.printf("Geben Sie noch einen Wert an: ");
+            no_2 = scanner.nextInt();
+
+            int result = no_1 + no_2;
+
+            System.out.println("Ihr Ergebnis ist: " + result);
+        }
+        catch (Exception ex)
+        {
+            System.out.println("Fehler");
+        }
     }
 }
